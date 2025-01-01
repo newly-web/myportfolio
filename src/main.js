82169c7ac8029bfece6d62b591052092a3ccfd6f
Whitney Nsegbe <whitneynsegbe@gmail.com>
2025-01-01 02:40:19 -0500

    finished header and worked of featured section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
-
+import'../styles/components/header.css'
+import '../styles/components/about.css'
+import '../styles/components/featured.css'
 import '../styles/utils.css'
