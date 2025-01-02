618ba620f0bd7ecfa9926ac80ccb2b5cd4368d40
Whitney Nsegbe <whitneynsegbe@gmail.com>
2025-01-01 22:46:26 -0500

    Finished project section and started adding accurate images + links

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import '../styles/style.css'
 import'../styles/components/header.css'
 import '../styles/components/about.css'
 import '../styles/components/featured.css'
+import'../styles/components/work.css'
+
+// Everything before this
 import '../styles/utils.css'
