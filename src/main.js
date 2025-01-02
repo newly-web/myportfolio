e84d3c941b9b9a29d77029192499455add8cf778
Whitney Nsegbe <whitneynsegbe@gmail.com>
2025-01-02 16:05:22 -0500

    finished website, aded all png files, fixed navbar

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import '../styles/modern-normalize.css'
-import '../styles/style.css'
-import'../styles/components/header.css'
-import '../styles/components/about.css'
-import '../styles/components/featured.css'
-import'../styles/components/work.css'
-
+import '/styles/modern-normalize.css'
+import '/styles/style.css'
+import'/styles/components/header.css'
+import '/styles/components/about.css'
+import '/styles/components/featured.css'
+import'/styles/components/work.css'
+import '/styles/components/contact.css'
+import'/styles/components/footer.css'
+import '/styles/components/mobilenavbar.css'
 // Everything before this
 import '../styles/utils.css'
+// js here
+import mobileNavigation from './utils/mobille-nav' 
+mobileNavigation()
---
+++ src/utils/mobille-nav.js
@@ -0,0 +1,30 @@
+const mobileNavigation = () => {
+    const headerBars = document.querySelector(".header__bars");
+    const mobileNav = document.querySelector(".mobile__nav");
+    const mobileLinks = document.querySelectorAll(".mobile__nav__link");
+
+    // state
+    let isMobileNavOpen = false;
+
+    headerBars.addEventListener('click', () => {
+        if (isMobileNavOpen == false) {
+            isMobileNavOpen = !isMobileNavOpen
+            mobileNav.style.display = "flex";
+            document.body.style.overflowY = "hidden";
+        }else{
+                mobileNav.style.display = "none";
+                isMobileNavOpen = false;
+                document.body.style.overflowY = "auto";
+
+            }
+
+    });
+    mobileLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            isMobileNavOpen = false;
+            mobileNav.style.display = "none";
+            document.body.style.overflowY = "auto";
+        });
+    });
+}
+export default mobileNavigation
